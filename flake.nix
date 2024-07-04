cd345e966e544a54414c81040e5b05d692055c44
Louis Dutton <louis.dutton@travelchapter.com>
2024-07-04 21:35:30 +0000

    chore: remove flake-parts and export generic lib

--- flake.nix
+++ flake.nix
@@ -3,45 +3,36 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      perSystem =
-        { pkgs, ... }:
-        {
-          packages.default =
-            let
-              toml = pkgs.formats.toml { };
-            in
-            {
-              generate =
-                {
-                  name,
-                  theme,
-                  cfg,
-                }:
-                pkgs.stdenv.mkDerivation {
-                  inherit name;
-                  dontInstall = true;
-                  src = theme;
-                  buildInputs = with pkgs; [ hugo ];
-                  buildPhase = ''
-                    hugo new site tmp
-                    mkdir -p tmp/themes/${cfg.theme}
-                    cp -r $src/* tmp/themes/${cfg.theme}
-                    hugo -s tmp -c ${toml.generate "hugo.toml" cfg} -d $out --noBuildLock
-                  '';
-                };
-            };
-        };
+    { nixpkgs, ... }:
+    {
+      lib = {
+        generate =
+          {
+            name,
+            theme,
+            cfg,
+            system,
+          }:
+          let
+            pkgs = import nixpkgs { inherit system; };
+            toml = pkgs.formats.toml { };
+            config = toml.generate "hugo.toml" cfg;
+          in
+          pkgs.stdenv.mkDerivation {
+            inherit name;
+            dontInstall = true;
+            src = theme;
+            buildInputs = with pkgs; [ hugo ];
+            buildPhase = ''
+              hugo new site tmp
+              mkdir -p tmp/themes/${cfg.theme}
+              cp -r $src/* tmp/themes/${cfg.theme}
+              hugo -s tmp -c ${config} -d $out --noBuildLock
+            '';
+          };
+      };
     };
 }
